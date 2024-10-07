36471833051a2ecf4148357596e2c33fb686cfe2
Daniela Homobono <110701158+danielaHomobono@users.noreply.github.com>
2024-10-07 11:54:32 -0300

    gestión clientes

--- app/src/main/java/com/example/androidclubdeportivo/GestionCliente.kt
+++ app/src/main/java/com/example/androidclubdeportivo/GestionCliente.kt
@@ -9,12 +9,8 @@ import androidx.core.view.WindowInsetsCompat
 class GestionCliente : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_gestion_cliente)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
     }
 }
