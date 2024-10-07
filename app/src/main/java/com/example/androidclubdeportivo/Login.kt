593c314d01705775e583b08c35c676d9d83887a2
Daniela Homobono <110701158+danielaHomobono@users.noreply.github.com>
2024-10-07 18:07:23 -0300

    clases kotlin

--- app/src/main/java/com/example/androidclubdeportivo/Login.kt
+++ app/src/main/java/com/example/androidclubdeportivo/Login.kt
@@ -1,16 +1,53 @@
 package com.example.androidclubdeportivo
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.EditText
+import android.widget.CheckBox
+//import androidx.navigation.findNavController
 
 class Login : AppCompatActivity() {
+
+    private lateinit var etUser: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var cbRememberMe: CheckBox
+    private lateinit var btnSubscribe: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_login)
+        etUser = findViewById(R.id.etUser)
+        etPassword = findViewById(R.id.etPassword)
+        cbRememberMe = findViewById(R.id.cbRememberMe)
+        btnSubscribe = findViewById(R.id.btnSubscribe)
+
+        // Set click listener for the subscribe button
+
+
+                val btnSubscribe = findViewById<Button>(R.id.btnSubscribe)
+                btnSubscribe.setOnClickListener{
+                    if (validateInput()) {
+                    val intent= Intent(this, Home::class.java)
+                    startActivity(intent)
+            } else {
+                Toast.makeText(this, "Por favor, complete todos los campos", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun validateInput(): Boolean {
+        val username = etUser.text.toString().trim()
+        val password = etPassword.text.toString().trim()
+
+        return username.isNotEmpty() && password.isNotEmpty()
+    }
 
     }
-}
+
