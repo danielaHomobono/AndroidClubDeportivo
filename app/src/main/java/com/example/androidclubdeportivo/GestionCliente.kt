4adfa7796fe90617990c05de9b355e55fd747180
Daniela Homobono <110701158+danielaHomobono@users.noreply.github.com>
2024-10-09 21:22:50 -0300

    cambio de icono

--- app/src/main/java/com/example/androidclubdeportivo/GestionCliente.kt
+++ app/src/main/java/com/example/androidclubdeportivo/GestionCliente.kt
@@ -3,17 +3,23 @@ package com.example.androidclubdeportivo
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class GestionCliente : AppCompatActivity() {
+    private lateinit var btnHome: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gestion_cliente)
 
+
         setupUI()
+        setupHomeButton()
+
     }
 
     private fun setupUI() {
@@ -39,6 +45,7 @@ class GestionCliente : AppCompatActivity() {
 
 
 
+
         val sucursalCerro = findViewById<LinearLayout>(R.id.sucursal_cerro)
         sucursalCerro.setOnClickListener {
             sendEmail("sucursal.cerro@example.com")
@@ -49,7 +56,20 @@ class GestionCliente : AppCompatActivity() {
             sendEmail("sucursal.cordoba@example.com")
         }
     }
-
+    private fun setupHomeButton() {
+        btnHome = findViewById(R.id.homeButton)
+        btnHome.setOnClickListener {
+            // Crear un Intent para volver a la actividad Home
+            val intent = Intent(this, Home::class.java)
+            // Limpiar la pila de actividades y poner Home en la parte superior
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+            startActivity(intent)
+            // Opcional: agregar una animación de transición
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            // Finalizar la actividad actual
+            finish()
+        }
+    }
     private fun sendEmail(email: String) {
         val intent = Intent(Intent.ACTION_SEND).apply {
             type = "message/rfc822"
